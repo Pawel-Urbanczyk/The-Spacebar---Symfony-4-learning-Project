67d7f045e42540079f1bbe84b4193d6c7226f8a3
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-24 13:45:52 +0200

    added functional editing of articles

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -33,7 +33,7 @@ class ArticleAdminController extends AbstractController
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-            /** @var Article $article*/
+            /** @var Article $article */
             $article = $form->getData();
 
             $em->persist($article);
@@ -51,13 +51,29 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article/{id}/edit")
+     * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, Request $request, EntityManagerInterface $em)
     {
+        $form = $this->createForm(ArticleFormType::class, $article);
 
-        dd($article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article Updated!');
+
+            return $this->redirectToRoute('admin_article_edit',[
+                'id'=>$article->getId()
+            ]);
+        }
+
+        return $this->render('article_admin/edit.html.twig',[
+            'articleForm'=>$form->createView(),
+        ]);
     }
 
     /**
