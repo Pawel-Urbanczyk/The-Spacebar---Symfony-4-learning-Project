022d4bdd55efda8c1a2a2d2474722ba774ae830a
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-24 11:34:05 +0200

    Improve ArticleFormType, added bootstrap theme on twig.yaml

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -33,17 +33,16 @@ class ArticleAdminController extends AbstractController
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-            $data = $form->getData();
-            $article = new Article;
-            $article->setTitle($data['title']);
-            $article->setContent($data['content']);
+            /** @var Article $article*/
+            $article = $form->getData();
             $article->setAuthor($this->getUser());
-            $article->setHeartCount(3);
 
             $em->persist($article);
             $em->flush();
 
-            return $this->redirectToRoute('app_homepage');
+            $this->addFlash('success', 'Article Created!');
+
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig',[
@@ -63,7 +62,7 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article", name="")
+     * @Route("/admin/article", name="admin_article_list")
      */
     public function list(ArticleRepository $articleRepository)
     {
