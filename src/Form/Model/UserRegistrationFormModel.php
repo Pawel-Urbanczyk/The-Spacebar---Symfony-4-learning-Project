39c704fb639cd60f2bffb518599466d90d3e6a86
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-28 13:22:51 +0200

    add data transfer object with using UserRegistrationFormModel and modify validation

---
+++ src/Form/Model/UserRegistrationFormModel.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Form\Model;
+
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="I think you've already registered!"
+ * )
+ */
+class UserRegistrationFormModel
+{
+    /**
+     * @Assert\NotBlank(message="Please enter an email!")
+     * @Assert\Email()
+     */
+    public $email;
+
+    /**
+     * @Assert\NotBlank(message="Choose a password")
+     * @Assert\Length(min=5, minMessage="Come on, you can think of a password longer than that...")
+     */
+    public $plainPassword;
+
+    /**
+     * @Assert\IsTrue(message="I know it' silly, but you must agree with terms")
+     */
+    public $agreeTerms;
+
+
+}
--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\Model\UserRegistrationFormModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -21,35 +22,15 @@ class UserRegistrationFormType extends AbstractType
             ->add('email', EmailType::class)
             //dont use password, avoid EVER setting that on a
             //field that might be persisted
-            ->add('plainPassword', PasswordType::class,[
-                'mapped'=>false,
-                'constraints'=>[
-                    new NotBlank([
-                        'message'=>'Choose a password!'
-                    ]),
-                    new Length([
-                        'min'=>5,
-                        'minMessage'=>'Come on, you can think of a password longer than that...',
-                        'max'=>15,
-                        'maxMessage'=>'Oh boy! that password is too long...'
-                    ])
-                ]
-            ])
-            ->add('agreeTerms',CheckboxType::class,[
-                'mapped'=>false,
-                'constraints'=>[
-                    new IsTrue([
-                        'message'=>'I know it\' silly, but you must agree with terms'
-                    ])
-                ]
-            ])
+            ->add('plainPassword', PasswordType::class)
+            ->add('agreeTerms',CheckboxType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => UserRegistrationFormModel::class,
         ]);
     }
 }
