0d3e1a61ceeedc081e53bf960993cc683fe7acce
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-28 13:03:48 +0200

    added two options in form, first disable author while editing and second remove publishedAt field when you want to add an areticle

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -28,6 +28,10 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $article = $options['data'] ?? null;
+        $isEdit = $article && $article->getId();
+
+
         $builder
             ->add('title', TextType::class,[
                 'help'=>'Choose something catchy!',
@@ -37,17 +41,23 @@ class ArticleFormType extends AbstractType
             ->add('content', null,[
                 'rows' => 15
             ])
-            ->add('publishedAt', null, [
-                'widget' => 'single_text'
+            ->add('author',UserSelectTextType::class, [
+                'disabled'=>$isEdit
             ])
-            ->add('author',UserSelectTextType::class)
         ;
+
+        if($options['include_published_at']){
+            $builder->add('publishedAt', null, [
+                'widget' => 'single_text'
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-           'data_class'=>Article::class
+           'data_class'=>Article::class,
+            'include_published_at' =>false
         ]);
     }
 
