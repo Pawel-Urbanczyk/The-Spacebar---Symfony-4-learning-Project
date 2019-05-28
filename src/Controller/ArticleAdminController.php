6616bf0f94115e5537ff4e834c98b0c4aa499ed8
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-28 17:41:57 +0200

    added some complex dynami Dependent Select Fields using ajax

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -20,7 +20,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class ArticleAdminController extends AbstractController
+class ArticleAdminController extends BaseController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
@@ -75,6 +75,30 @@ class ArticleAdminController extends AbstractController
 
         return $this->render('article_admin/edit.html.twig',[
             'articleForm'=>$form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/article/location-select", name="admin_article_location_select")
+     * @IsGranted("ROLE_USER")
+     */
+    public function getSpecificLocationSelect(Request $request)
+    {
+        if(!$this->isGranted('ROLE_ADMIN_ARTICLE') && $this->getUser()->getArticles()->isEmpty()){
+            throw $this->createAccessDeniedException();
+        }
+
+        $article = new Article();
+        $article->setLocation($request->query->get('location'));
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        //no field? return empty respone
+        if(!$form->has('specificLocationName')){
+            return new Response(null, 204);
+        }
+
+        return $this->render('article_admin/_specific_location_name.html.twig', [
+            'articleForm' => $form->createView(),
         ]);
     }
 
