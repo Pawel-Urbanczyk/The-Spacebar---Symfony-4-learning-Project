441c092427042cf0e31344dd984f253db37639f1
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-06-09 17:29:52 +0200

    testing new article form

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -16,4 +16,25 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertStatusCode(200, $client);
     }
+
+    public function testArticleAddUsingForm()
+    {
+        $client = $this->makeClient();
+        $client->followRedirect();
+
+        $crawler = $client->request('GET', '/admin/article/new');
+        $this->assertStatusCode(200, $client);
+
+        $form = $crawler->selectButton('Submit')->form();
+        $form['article_form[title]']->setValue('Letters');
+        $form['article_form[content]']->setValue('ABCD');
+        $form['article_form[location]']->select(2);
+        $form['article_form[specificLocationName]']->select(2);
+        $form['article_form[author]']->setValue('admin0@spacebar.com');
+
+        $client->submit($form);
+
+        $this->assertContains('Article Created!', $client->getResponse()->getContent());
+
+    }
 }
